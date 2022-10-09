f168faa628275a7e4e648139d599f670ef981521
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2022-10-09 12:28:48 +0530

    initial commit

--- keys.js
+++
@@ -1,7 +0,0 @@
-// API Keys = https://developers.google.com/custom-search/v1/using_rest
-
-export const API_KEY = 'AIzaSyAOnLuIFF_d9eKhJNP4HR8TKKzy-x6584A'
-
-// Context key - https://cse.google.com/cse/create/new
-
-export const CONTEXT_KEY = '93f822ad204b14a9d'
