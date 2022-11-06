f943c488d652973b99dbea57b6a1514af6b42336
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2022-11-06 12:59:52 +0530

    added alt

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           height={100}
           width={300}
-          
+          alt=""
         />
 
         {/* Search */}
